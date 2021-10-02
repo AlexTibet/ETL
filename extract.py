31b163305a0e8b1f258ec09e85a4157c3433c53f
Alexandr Sobko <alexfreetibet@gmail.com>
2021-10-02 15:47:28 +0700

    create class for extract data

--- extract.py
+++ extract.py
@@ -1,29 +1,58 @@
 import csv
 import json
 import xml.etree.ElementTree as ElementTree
+import os
 
 
-def read_csv(file_path_csv):
-    with open(file_path_csv, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            print(row)
+class Extractor:
+    path: str = None
+    data = []
 
+    def __init__(self, path: str = os.getcwd()):
+        print(path)
+        abs_path = os.path.abspath(path)
+        normal_path = os.path.normpath(abs_path)
+        self.path = os.path.join(normal_path, 'input_files')
 
-def read_json(file_path_json):
-    with open(file_path_json) as json_file:
-        data = json.load(json_file)
-        for row in data['fields']:
-            print(row)
+    def extract_data(self):
+        readers = {
+            '.csv': self._read_csv,
+            '.json': self._read_json,
+            '.xml': self._read_xml,
+        }
+        files = os.listdir(self.path)
 
+        for file in files:
+            filename, file_extension = os.path.splitext(file)
+            filepath = os.path.join(self.path, file)
+            readers[file_extension](filepath)
 
-def read_xml(xml_file_path):
-    tree = ElementTree.parse(xml_file_path)
-    root = tree.getroot()
+        return self.data
 
-    for object_main_tag in root.findall('objects'):
+    def _read_csv(self, filepath: str):
+        with open(filepath, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                self.data.append(row)
+
+    def _read_json(self, filepath: str):
+        with open(filepath) as json_file:
+            data = json.load(json_file)
+            for row in data['fields']:
+                self.data.append(row)
+
+    def _read_xml(self, filepath: str):
+        tree = ElementTree.parse(filepath)
+        root = tree.getroot()
+
+        for object_main_tag in root.findall('objects'):
+            self._get_data_from_xml_row(object_main_tag)
+
+    def _get_data_from_xml_row(self, object_main_tag: ElementTree.Element):
         data = dict()
         object_tag = object_main_tag.findall('object')
+
         for part in object_tag:
             data[part.attrib['name']] = part.find('value').text
-        print(data)
+
+        self.data.append(data)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import extract as ex
+import os
+
 
 file_path_csv_one = "./input_files/csv_data_1.csv"
 file_path_csv_two = "./input_files/csv_data_2.csv"
@@ -8,7 +10,15 @@ file_path_xml = "./input_files/xml_data.xml"
 
 if __name__ == '__main__':
     print('Hello world')
-    ex.read_csv(file_path_csv_one)
-    ex.read_csv(file_path_csv_two)
-    ex.read_json(file_path_json)
-    ex.read_xml(file_path_xml)
+    # csv_one = ex.read_csv(file_path_csv_one)
+    # csv_two = ex.read_csv(file_path_csv_two)
+    # json_data = ex.read_json(file_path_json)
+    # xml_data = ex.read_xml(file_path_xml)
+
+    # extract_data = [*csv_one, *csv_two, *json_data, *xml_data]
+    # print(extract_data)
+
+    extractor = ex.Extractor()
+    data = extractor.extract_data()
+    print(data)
+
