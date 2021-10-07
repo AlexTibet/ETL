249b270fd6b2b92dbfbe233e7f43b9075f919ad1
Alexandr Sobko <alexfreetibet@gmail.com>
2021-10-08 00:38:19 +0700

    update extractor

---
+++ extract/extract.py
@@ -0,0 +1,36 @@
+import os
+from extract.readers import CSVReader, JSONReader, XLSReader, Reader
+from typing import Type
+
+
+class Extractor:
+    path: str
+    data: list
+    _readers: dict[str, Type[Reader]]
+    default_path = os.path.join(os.getcwd(), 'input_files')
+
+    def __init__(self, path: str = default_path):
+        self.path = path
+        self.data = []
+        self._readers = {
+            '.csv': CSVReader,
+            '.json': JSONReader,
+            '.xml': XLSReader,
+        }
+        print(self.path)
+
+    def get_data(self, path: str = default_path) -> [dict]:
+        files = os.listdir(path)
+
+        for file in files:
+            self._extract_data(file, path)
+
+        return self.data
+
+    def _extract_data(self, file, path):
+        filename, file_extension = os.path.splitext(file)
+        filepath = os.path.join(path, file)
+
+        reader = self._readers[file_extension]()
+        self.data.extend(reader.get_data(filepath))
+
