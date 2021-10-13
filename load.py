3b5da46ab70d0100b7023a073c24901da83a0ab2
Alexandr Sobko <alexfreetibet@gmail.com>
2021-10-13 20:24:22 +0700

    create simple loader

--- load.py
+++ load.py
@@ -1,8 +1,23 @@
 import csv
 
 
-def load_to_tsv(output_file_name, data: []):
-    with open(output_file_name, 'w') as tsvfile:
-        writer = csv.writer(tsvfile, delimiter='\t', newline='\n')
-        for row in data:
-            writer.writerow(row)
+def get_fieldnames(data):
+    fieldnames = []
+    for row in data:
+        for key in row.keys():
+            fieldnames.append(key)
+    return fieldnames
+
+
+def load_to_tsv(output_file_name, data: []) -> bool:
+    try:
+        with open(output_file_name, 'wt') as tsv_file:
+            fieldnames = get_fieldnames(data)
+            writer = csv.DictWriter(tsv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+    except Exception as err:
+        print(err)
+        return False
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from extract import Extractor
 import os
+from load import load_to_tsv
 
 
 if __name__ == '__main__':
@@ -7,4 +8,5 @@ if __name__ == '__main__':
     extractor = Extractor()
     data = extractor.get_data()
     print(data)
+    load_to_tsv('test_output.tsv', data)
 
